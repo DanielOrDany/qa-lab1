aa647f9bf144cde4caa5317a15058952d32bb7ef
DanielOrDany <danylo.n@volpis.com>
2021-11-08 01:39:59 +0200

    remake it to class exm

--- lab.py
+++ lab.py
@@ -3,20 +3,57 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import chromedriver_binary
 
-#Open your browser (Chrome 94.0.4606.41)
-driver = webdriver.Chrome()
+class WebSiteTest:
+  def __init__(self, website_name):
+    self.website_name = website_name
+    self.chrome_driver = webdriver.Chrome()
 
-#Open the Google search TOP screen.
-driver.get("https://www.google.com")
+  def findSiteInGoogle(self):
+    print("Website: " + self.website_name)
 
-#Type "quickquery" as the search term and press Enter.
-search = driver.find_element_by_name('q')
-search.send_keys("quickquery")
-search.send_keys(Keys.ENTER)
+    # Open your browser (Chrome 94.0.4606.41)
+    driver = self.chrome_driver
 
-element = driver.find_element_by_partial_link_text("QuickQuery")
-element.click()
+    # Open the Google search TOP screen.
+    driver.get("https://www.google.com")
 
-sleep(5)
+    search = driver.find_element_by_name('q')
+    search.send_keys(self.website_name)
+    search.send_keys(Keys.ENTER)
 
-driver.close()
+    element = driver.find_element_by_partial_link_text(self.website_name)
+    element.click()
+
+    sleep(5)
+    driver.close()
+
+  def registerNewUser(self):
+    print("Website: " + self.website_name)
+
+    # Open your browser (Chrome 94.0.4606.41)
+    driver = self.chrome_driver
+
+    # Open the Google search TOP screen.
+    driver.get("https://www.google.com")
+
+    search = driver.find_element_by_name('q')
+    search.send_keys(self.website_name)
+    search.send_keys(Keys.ENTER)
+
+    element = driver.find_element_by_partial_link_text(self.website_name)
+    element.click()
+
+    signInBtn = driver.find_element_by_xpath('//a[contains(text(), "Signin")]')
+    signInBtn.click()
+
+    createNewAccountBtn = driver.find_element_by_xpath('//a[contains(text(), "Create")]')
+    createNewAccountBtn.click()
+
+    sleep(5)
+    driver.close()
+
+web_site = WebSiteTest("testapp")
+
+# web_site.findSiteElement();
+
+web_site.registerNewUser()
